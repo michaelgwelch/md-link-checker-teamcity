a4206c1eb31a88262e2b98cee9f448da42acb9d1
Michael Welch <michaelgwelch@gmail.com>
2017-11-08 16:25:03 -0600

    Esacpe space characters in baseUrl

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ function markdownLinkCheck(file, opts) {
 
 async function linkCheckFile(file) {
   try {
-    const fullPath = path.join('file:///', cwd, file);
+    const fullPath = path.join('file:///', cwd, file).replace(' ', '\\ ');
+    console.log(fullPath);
     const opts = { baseUrl: path.dirname(fullPath) };
     const content = await readFile(file, { encoding: 'utf8' });
     const allLinks = await markdownLinkCheck(content, opts);
